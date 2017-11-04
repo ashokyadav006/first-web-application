677728ce5231fd4e4923a8f63ad7b9e0737be243
Ashok Yadav <yadaas@amazon.com>
2017-11-04 10:36:07 +0530

    Added JSP

---
+++ src/main/java/yadav/ashok/LoginServlet.java
@@ -0,0 +1,19 @@
+package yadav.ashok;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/login.do")
+public class LoginServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+	}
+}
